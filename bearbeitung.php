2e2def59a4362b2dd7d75bfa6ba89c48eae61541
on22-neleries <nel.ries.22@lehre.mosbach.dhbw.de>
2023-11-26 14:57:53 +0100

    Überprüfen, ob ID im URL-Parameter vorhanden ist

--- bearbeitung.php
+++ bearbeitung.php
@@ -2,4 +2,17 @@
 include('Kalender.php');
 include('config.php');
 
+// Überprüfen, ob die ID im URL-Parameter vorhanden ist
+
+if(isset($_GET['id'])){
+    session_start();
+    if(!isset($_SESSION['uid']))
+        header("Location: index.php");
+    $appointmentData = Calendar::fetchAppointmentById($_GET['id'], $con);
+} else {
+    die("Keine ID angegeben!");
+}
+
+if (empty($appointmentData))
+    die("Der gesuchte Termin konnte nicht gefunden werden!");
 ?>
