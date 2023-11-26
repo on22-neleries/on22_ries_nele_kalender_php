ea3893e98b163b2307415edf737585e932a0ff38
on22-neleries <nel.ries.22@lehre.mosbach.dhbw.de>
2023-11-26 14:35:49 +0100

    HTML head

--- update.php
+++ update.php
@@ -27,3 +27,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Calender</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
